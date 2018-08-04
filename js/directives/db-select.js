738730ab3ab05743bd37b2015b453c0bfb20bf7e
Adolfo Carvalho <adolfotcar@gmail.com>
2018-08-04 17:48:56 +0100

    Fixed bug that was preventing it from loading properly if the field was made blank Added comments

--- js/directives/db-select.js
+++ js/directives/db-select.js
@@ -4,24 +4,20 @@ angular
 		return {
 			restrict: 'EA',
 			scope: {
-				backend: '@',
-				idName: '@',
-				name: '@',
-				backend: '@',
-				fieldBind: '@',
-				textPlaceholder: '@'
+				backend: '@', //URL of the backend, it will be appended to the backend in the Api
+				idName: '@', //when opening an item this will be ID
+				name: '@', //when opening an item this will be the label
+				fieldBind: '@', //field in the parent directive which will be bound to this directive
+				textPlaceholder: '@' //placeholder
 			},
 			templateUrl: 'app/templates/db-select.html',
 			controller: function($scope, $http, Api){
-
-				//sometimes the parent directive needs making the values of this directive completely blank
+				//this should be broadcasted from the parent directive if needed to make the select empty
 				$scope.$on('makeBlank', function(event){
 					$scope.select = {};
 					$scope.data = {};
+					$scope.idName = $scope.name = '';
 					$scope.options = [{'id': '', 'name': ''}];
-					$scope.select.selected = $scope.options[0];
-					$scope.select.selected.name = "";
-					$scope.$$childTail.select.selected = {};
 				});
 
 				$scope.search = function(search){
@@ -31,12 +27,13 @@ angular
 							url: Api.backend+$scope.backend+'?token='+Api.getToken()+'&search='+search
 						})
 						.success(function(response){
-							//the desired response must be a json element called list
 							if (response.data) {
 								var list = response.data.list;
+								//it accepts a list from the backend containing elements in the following formats:
+								//{id: 1, name: 'A'} or
+								//{id:1, title: 'A'}
 								angular.forEach(list, function(item){
-									//in case the backend sends title instead of name
-									item.name = item.name? item.name : item.title;
+									item.name = item.name ? item.name : item.title;
 								});
 								$scope.options = response.data.list;
 							}
@@ -45,16 +42,23 @@ angular
 				};
 
 				$scope.selected = function(id) {
-					//after selecting an item, sends the updateItem call to the parent directive
+					//emits the signal to the parent directive that it must update the value of the bound field
 					$scope.$emit('updateItem', $scope.fieldBind, id);
 					angular.forEach($scope.options, function(item){
-						if (item.id==id) $scope.data = item;
+						if (item.id==id) {
+							$scope.data = item;
+							return; //skipping foreach once the element is found
+						}
 					});
+					//each option can be an element with various attributes
+					//and sometimes this directive is responsible for loading all the information into the form (which is the select parent directive)
+					//sending the signal to the parent directive (the form) to update the data
 					$scope.$emit('setData', $scope.data);
 				}
 			},
 			link: function(scope){
-				//as it may take a while until the directive is built, keeps watching the value of name...when it's filled then it creates options with a single item and makes it the selected one
+				//as the name most likely will be an angular variable, it may take a while until it's ready...
+				//so keeps and eye on the name until it has a value, and when it gets that value creates the array of options with only one element
 				scope.$watch('name', function(value){
 					if (value!='') {
 						scope.select = {};
